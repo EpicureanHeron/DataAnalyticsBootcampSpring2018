908015912990c2811e33f0a08a16025673a6c480
Dylan Rossi <ddrossi93@gmail.com>
2018-02-20 19:18:43 -0600

    It got rid of the data folder for some reason... I'm confused

--- Week 5 - MatPlotLib/practices/06-Stu_Standard_Error/Unsolved/samples_challenge.py
+++
@@ -1,9 +0,0 @@
-# Dependencies
-from matplotlib import pyplot as plt
-import numpy as np
-import pandas as pd
-
-
-# Read and shuffle data
-housing_data = pd.read_csv("../Resources/housing_data.csv")
-housing_data = housing_data.sample(frac=1).reset_index(drop=True)
--- Week 5 - MatPlotLib/practices/10-Stu_Fits_and_Regression/Unsolved/crime.py
+++
@@ -1,9 +0,0 @@
-# Dependencies
-from matplotlib import pyplot as plt
-from scipy import stats
-
-import numpy as np
-import pandas as pd
-
-
-crime_data = pd.read_csv("../Resources/crime_data.csv")
