31eebfecb0d320f7e7c26e98d2964f34db1c8fa7
Dylan Rossi <ddrossi93@gmail.com>
2018-04-16 21:30:52 -0500

    finished lessons

--- Week 15 - JavaScript/Class 1/13-Stu_Events/Unsolved/index.js
+++ Week 15 - JavaScript/Class 1/13-Stu_Events/Unsolved/index.js
@@ -1,10 +1,15 @@
 // Set variable for the button
+var clickButton = document.querySelector("#clickHere");
 
 // Add click even listener to the button
+clickButton.addEventListener("click", function() {
+  // pop is an Array method which removes the last element
+
   // Prompt user for favorite color
-
+  userColor = prompt("What is your favorite color?: ");
   // Create paragraph element
-
-  // Select the id tag for color
+  paragraph = document.querySelector("#color");
 
   // Add new html line to color
+  paragraph.innerHTML = "Favoite Color: " + userColor;
+});
